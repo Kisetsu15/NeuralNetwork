46d08292bb5ea3e5bc6eaf50169aa33f07bebe92
Kisetsu <dharshik06@gmail.com>
2025-03-24 21:14:24 +0530

    Delete Softmax.cs

--- Softmax.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NeuralNetwork {
-    public class Softmax : IOutputFunction {
-        public List<float> Compute( List<float> list ) {
-            float sum = list.Select(x => MathF.Exp(x)).Sum();
-            return [.. list.Select(z => MathF.Exp(z) / sum)];
-        }
-    }
-}
